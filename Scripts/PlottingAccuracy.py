883a38cc7cd1d8cac6cba3e445e2492aa496477f
mahmoudgamalfcis55 <mahmoudgamalfcis55@gmail.com>
2020-01-11 19:46:55 +0200

    First Commit

---
+++ Scripts/PlottingAccuracy.py
@@ -0,0 +1,3 @@
+#--------------------------------PLOTTING------------------------------------
+def Plotting_Accuracy(models_name ,Lis):
+  plt.bar(models_name,AccuracyList)
---
+++ Scripts/PlottingTraining.py
@@ -0,0 +1,4 @@
+models_name=['Logistic'+'\n'+'Regression' ,'Poly'+'\n'+'Logistic'+'\n'+'d=2','Poly'+'\n'+'Logistic'+'\n'+'d=3','Decision'+'\n'+'Tree',' AdaBoost'+'\n'+'(Decision'+'\n'+'Tree)',
+             'SVM'+'\n'+'  Gaussian'+'\n'+'OVR','SVM'+'\n'+'  Gaussian'+'\n'+'OVO','KNN']
+
+Plotting_TrainingTime(models_name,AccuracyList)
